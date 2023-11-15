bdf31dceeb63563c4880428a31b4b38691750297
odevpedro <pedrosschmidt2@gmail.com>
2023-11-15 09:10:55 -0300

    first cae bank transactions

---
+++ src/main/java/com/odevpedro/admin/catalogo/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,27 @@
+package com.odevpedro.admin.catalogo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+public class BankTransactionAnalyzerSimple {
+
+    private  static final String RESSOURCES = "src/main/resources/something.csv";
+    public static void main(String[] args) throws IOException {
+        final Path path = Paths.get(RESSOURCES + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        for (final String line : lines){
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total for all transactions is " + total);
+
+    }
+
+
+
+}
